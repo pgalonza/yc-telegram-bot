890284fd9450b8464d00a57e8183c9dd9b4f5ec3
Peter Galonza <p.galonza@evaron.ru>
2024-08-10 14:45:01 +0300

    feature: add asyncio

--- bot/index.py
+++ bot/index.py
@@ -1,4 +1,5 @@
 import telebot
+import telebot.async_telebot
 import os
 import logging
 import random
@@ -7,13 +8,14 @@ import base64
 import json
 import io
 import time
+import asyncio
 
 
 API_TOKEN = os.environ['TELEGRAM_TOKEN']
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
-bot = telebot.TeleBot(API_TOKEN, threaded=False)
+bot = telebot.async_telebot.AsyncTeleBot(API_TOKEN)
 folder_id = None
 aim_header = None
 logger_interface = None
@@ -44,7 +46,7 @@ def get_folder_id(iam_token, version_id):
     folder_id = folder_id_data['folderId']
     return folder_id
 
-def handler(event, context):
+async def handler(event, context):
     global folder_id, logger_interface
     logger_interface = logging.getLogger('bot')
     logging_configuration(logger_interface)
@@ -54,19 +56,19 @@ def handler(event, context):
 
     message = telebot.types.Update.de_json(event['body'])
     logger_interface.info('\rMessage text: %s,\rSender: %s', message.message.text, message.message.from_user.username)
-    bot.process_new_updates([message])
+    await bot.process_new_updates([message])
     return {
         'statusCode': 200
     }
 
 
 @bot.message_handler(commands=['start',])
-def start_message(message):
+async def start_message(message):
     message_text = "Во славу императора!"
-    bot.reply_to(message, message_text)
+    await bot.reply_to(message, message_text)
 
 @bot.message_handler(commands=['genimage',])
-def yandex_art(message):
+async def yandex_art(message):
     prompt = {
         "modelUri": f"art://{folder_id}/yandex-art/latest",
         "generationOptions": {
@@ -86,18 +88,18 @@ def yandex_art(message):
     result = requests.post('https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsync', data=json.dumps(prompt), headers=aim_header)
     if result.status_code != 200:
         logger_interface.error(result.text)
-        bot.reply_to(message, "Не могу сгенерировать изображение")
+        await bot.reply_to(message, "Не могу сгенерировать изображение")
         return None
     result = result.json()
     while True:
         result = requests.get('https://llm.api.cloud.yandex.net:443/operations/' + result['id'], headers=aim_header)
         result = result.json()
         logger_interface.info('Image was ready: %s', result['done'])
-        bot.reply_to(message, "Шедевр рисуется.", disable_notification=True)
+        await bot.reply_to(message, "Шедевр рисуется.", disable_notification=True)
         if result['done']:
             encoded = result['response']['image']
             data = base64.b64decode(encoded)
-            bot.send_photo(message.chat.id, telebot.types.InputFile(io.BytesIO(data)), reply_parameters=telebot.types.ReplyParameters(message.message_id, message.chat.id, allow_sending_without_reply=True))
+            await bot.send_photo(message.chat.id, telebot.types.InputFile(io.BytesIO(data)), reply_parameters=telebot.types.ReplyParameters(message.message_id, message.chat.id, allow_sending_without_reply=True))
             break
 
         time.sleep(10)
